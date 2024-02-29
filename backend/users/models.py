855b0ec6bac6ae161bc2d63406f5de76ac5ab9a6
Igor Muzyka <muzyka-iv@yandex.ru>
2024-02-29 16:02:53 +0600

    added users

---
+++ backend/users/models.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    username = models.CharField(
+        verbose_name='Username',
+        max_length=150,
+        unique=True,
+        blank=False,
+    )
+
+
+class Author(User):
+    class Meta:
+        verbose_name = 'Author'
+        verbose_name_plural = 'Authors'
+        ordering = ['username']
+
+    def __str__(self):
+        return f'Author[{self.username}]'
+
+
+class Student(User):
+    class Meta:
+        verbose_name = 'Student'
+        verbose_name_plural = 'Students'
+        ordering = ['username']
+
+    def __str__(self):
+        return f'Student[{self.username}]'
